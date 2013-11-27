d0de894c8607158b0fdcd74a8d77064aa9c9a461
Cristian <cristian.658@gmail.com>
2013-11-27 09:46:13 -0300

    Se agrega Login al sistema (Falta conectarlo a la base de datos) Se agrega ListAccounts (Listado de cuenta, solo pantalla)

---
+++ AppAccounts/src/cl/ciisa/appaccounts/ListAccountActivity.java
@@ -0,0 +1,41 @@
+package cl.ciisa.appaccounts;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class ListAccountActivity extends Activity {
+
+	private Button agregarView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_list_account);
+		agregarView = (Button)findViewById(R.id.agregarView);
+	
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.list_account, menu);
+		return true;
+	}
+	
+	public void addListenerButton(){
+		agregarView.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				
+				
+			}
+		});
+	}
+
+}
